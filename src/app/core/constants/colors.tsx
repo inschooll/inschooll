7d2271189fafeb5a158431a1b0f23c5ebb4cf646
dc <00danzy@gmail.com>
2023-11-07 11:14:00 +0100

    first commit

---
+++ src/app/core/constants/colors.tsx
@@ -0,0 +1,24 @@
+// primary
+const blue = '#54A0FF';
+const blue2 = '#2E86DE';
+const darkGrey = '#404756';
+const purple = '#978EFF';
+
+// secondary
+const black = '#121212';
+const green = '#215B57';
+const pink = '#FF5F93';
+const grey = '#EDEDED';
+const lightGrey = '#f7f7f4'
+
+export default {
+  blue,
+  blue2,
+  darkGrey,
+  purple,
+  black,
+  green,
+  pink,
+  grey,
+  lightGrey,
+}
