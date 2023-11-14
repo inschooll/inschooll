8dfc85d746b72b17fc33c16918b0e3a6ff58a8bf
dc <00danzy@gmail.com>
2023-11-14 15:33:18 +0100

    first commit

---
+++ src/core/utils.ts
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+import links from "~/app/core/constants/links";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export async function getAuthProfile() {
+  const session = await getServerAuthSession();
+  const profile = session ? await api.profile.getProfileByUserId.query({userId: session?.user.id}) : null;
+  
+  return profile;
+}
+
+export async function protectPage() {
+  const session = await getServerAuthSession();
+  const profile = session ? await api.profile.getProfileByUserId.query({userId: session?.user.id}) : null;
+
+  if (!session?.user || !profile) redirect(links.login);
+  if (session?.user && !profile) redirect(links.completeRegistration);
+}
