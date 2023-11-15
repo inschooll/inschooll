11e54068a115335f2839a617450072af29fd5b2b
dc <00danzy@gmail.com>
2023-11-15 15:36:15 +0100

    add schoolRouter to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { profileRouter } from "./routers/profile";
+import { schoolRouter } from "./routers/school";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { profileRouter } from "./routers/profile";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   profile: profileRouter,
+  school: schoolRouter,
 });
 
 // export type definition of API
