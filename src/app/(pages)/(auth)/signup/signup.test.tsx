5fd918fddabc4c54b25334ad9be3ebf55fc55b79
dc <00danzy@gmail.com>
2023-11-17 22:33:13 +0100

    complete

---
+++ src/app/(pages)/(auth)/signup/signup.test.tsx
@@ -0,0 +1,8 @@
+import Login from '@/app/auth/login/page';
+import { fireEvent, render, screen} from '@testing-library/react';
+
+describe('Login Page ', () => {
+  it('should have form submit when all data in form is valid', () => {
+  });
+});
+
