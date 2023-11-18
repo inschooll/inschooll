3d7a12a7ac3eef1f5c6c60d38875553a01762ca4
dc <00danzy@gmail.com>
2023-11-18 22:37:39 +0100

    complete

---
+++ src/app/(pages)/pick-university/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '~/app/_components/navbar/navbar';
+import PickUniversityBody from './body';
+import { api } from '~/trpc/server';
+
+export default async function PickUniversity() {
+  const schools = await api.school.getAll.query({limit: 6, skip: 0});
+
+  console.log(schools);
+
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <div className='my-14 max-w-[1300px] mx-auto px-5 sm:px-10 lg:px-20 h-full'>
+          {/* Title */}
+          <h1 className="font-bold text-4xl sm:text-6xl">
+            Pick University
+          </h1>
+
+          <PickUniversityBody initialSchools={schools} />
+          
+        </div>
+      </main>
+
+    </>
+  );
+}
