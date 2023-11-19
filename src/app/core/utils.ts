ee8166281467ecd4a56d82b7b5fc4efe1d73f7e6
dc <00danzy@gmail.com>
2023-11-19 22:45:53 +0100

    improve code

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,10 +1,3 @@
-// import { clsx, ClassValue } from "clsx";
-// import { twMerge } from "tailwind-merge";
-
-// export function cn(...inputs: ClassValue[]) {
-//     return twMerge(clsx(inputs));
-// }
-
 export function getErrorMessage(msg: string) {
-    return 'An error occurred!';
+  return "An error occurred!";
 }
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -5,14 +5,18 @@ import { api } from "~/trpc/server";
 
 export async function getAuthProfile() {
   const session = await getServerAuthSession();
-  const profile = session ? await api.profile.getProfileByUserId.query({userId: session?.user.id}) : null;
-  
+  const profile = session
+    ? await api.profile.getProfileByUserId.query({ userId: session?.user.id })
+    : null;
+
   return profile;
 }
 
 export async function protectPage() {
   const session = await getServerAuthSession();
-  const profile = session ? await api.profile.getProfileByUserId.query({userId: session?.user.id}) : null;
+  const profile = session
+    ? await api.profile.getProfileByUserId.query({ userId: session?.user.id })
+    : null;
 
   if (!session?.user || !profile) redirect(links.login);
   if (session?.user && !profile) redirect(links.completeRegistration);
