94d11ab2104c8a756a188d784de910cf04cc0103
dc <00danzy@gmail.com>
2023-11-30 10:14:09 +0100

    first commit

---
+++ src/app/(pages)/[school]/style-adder.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from "react";
+
+{/* This fragment component helps add unique css properties to the body element when it mounts */}
+export default function StyleAdder() {
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+    const body = document.querySelector('body') as HTMLBodyElement;
+    // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+    const html = document.querySelector('html') as HTMLHtmlElement;
+    body.style.height = '100%';
+    html.style.height = '100%';
+    body.style.overflow = 'hidden';
+    html.style.overflow = 'hidden';
+  })
+  return (<></>);
+}
