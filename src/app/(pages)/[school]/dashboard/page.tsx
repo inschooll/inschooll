41c9809975b9b846882282ae4b911be022fb9236
dc <00danzy@gmail.com>
2023-12-07 21:36:59 +0100

    improve code

--- src/app/(pages)/[school]/dashboard/page.tsx
+++ src/app/(pages)/[school]/dashboard/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
 
-export default function Dashboard({params}: {params: {school: string}}) {
+
+export default function Dashboard({ params }: { params: { school: string } }) {
   return (
-    <div>Dashboard: {params.school}</div>
-  )
+    <div className="bg-green-5000 flex-grow overflow-auto">
+      <div></div>
+      {/* {Array.from({length: 50}, (_, index) => index).map((index) => (<p key={index}>{index}</p>))} */}
+    </div>
+  );
 }
