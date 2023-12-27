d9ddc7af22658a4f09a750c14a28f165de626af1
dc <00danzy@gmail.com>
2023-12-27 11:44:35 +0100

    style: change all components weight from `bold` to `semibold` as default

--- src/app/_components/texts/title.tsx
+++ src/app/_components/texts/title.tsx
@@ -4,33 +4,33 @@ interface TProps {
   weight?: "regular" | "medium" | "semibold" | "bold"
 }
 
-export function T1({children, weight="bold", color="text-cc-content-sub"} : TProps) {
+export function T1({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
   return (
     <h4 className={`font-${weight} text-5xl ${color}`}>{children}</h4>
   );
 }
-export function T2({children, weight="bold", color="text-cc-content-sub"} : TProps) {
+export function T2({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
   return (
     <h4 className={`font-${weight} text-4xl ${color}`}>{children}</h4>
   );
 }
-export function T3({children, weight="bold", color="text-cc-content-sub"} : TProps) {
+export function T3({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
   return (
     <h4 className={`font-${weight} text-3xl ${color}`}>{children}</h4>
   );
 }
-export function T4({children, weight="bold", color="text-cc-content-sub"} : TProps) {
+export function T4({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+  return (
+    <h4 className={`font-${weight} text-[26px] ${color}`}>{children}</h4>
+  );
+}
+export function T5({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
   return (
     <h4 className={`font-${weight} text-xl ${color}`}>{children}</h4>
   );
 }
-export function T5({children, weight="bold", color="text-cc-content-sub"} : TProps) {
+export function T6({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
   return (
     <h4 className={`font-${weight} text-lg ${color}`}>{children}</h4>
-  );
-}
-export function T6({children, weight="bold", color="text-cc-content-sub"} : TProps) {
-  return (
-    <h4 className={`font-${weight} text-base ${color}`}>{children}</h4>
   );
 }
