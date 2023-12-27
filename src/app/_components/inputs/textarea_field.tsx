c66887aef30aebe693ba74493ef28ca56186d790
dc <00danzy@gmail.com>
2023-12-27 11:49:01 +0100

    style: improve styles

--- src/app/_components/inputs/textarea_field.tsx
+++ src/app/_components/inputs/textarea_field.tsx
@@ -5,9 +5,9 @@ export type TextareaProps = DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAre
 export default function TextareaField({...props}: TextareaProps) {
   return (
     <textarea
-      rows={8}
+      rows={props.rows ?? 8}
       className={
-        "focus:border-1 py-2 resize-none w-full rounded border-2 bg-cc-input-bg px-2 text-cc-input-text outline-none transition-colors duration-200 placeholder:font-normal placeholder:text-cc-content-main/20 focus:border-cc-primary-main"
+        "focus:border-1 py-2 resize-none w-full rounded border-2 bg-cc-input-bg px-2 text-cc-input-text outline-none transition-colors duration-200 placeholder:font-normal placeholder:text-cc-content-main/20 border-cc-border-main focus:border-cc-primary-main"
       }
       {...props}
     />
