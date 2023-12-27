88d67f627bd2d132bbe011a661cdaa7159011cf0
dc <00danzy@gmail.com>
2023-12-27 11:59:36 +0100

    refactor: use new TopNavbar naming

--- src/app/(pages)/(auth)/layout.tsx
+++ src/app/(pages)/(auth)/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "~/app/_components/navbar/navbar";
+import TopNavbar from "~/app/_components/navbar/navbar";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
+      <TopNavbar />
       <div className="bg-red-4000 mx-auto my-5 mb-20 mt-10 flex min-h-[60vh] flex-col justify-center p-3 sm:w-96 sm:p-0">
           {children}
       </div>
