3acc52522433cd5d880dda5495af23bdf55f11a1
dc <00danzy@gmail.com>
2023-12-27 12:01:55 +0100

    refactor: use new TopNavbar naming

--- src/app/(pages)/(create-form)/create-school/page.tsx
+++ src/app/(pages)/(create-form)/create-school/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '~/app/_components/navbar/navbar'
+import TopNavbar from '~/app/_components/navbar/navbar'
 import { FormBody } from './components/FormBody'
 import { protectPage } from '~/core/utils';
 
@@ -8,7 +8,7 @@ export default async function CreateSchool() {
   
   return (
     <>
-      <Navbar />
+      <TopNavbar />
 
       <main className='mb-56 sm:mt-5 sm:px-10 md:max-w-3xl lg:max-w-4xl sm:mx-auto'>
           <FormBody />
