8e62cdee205bdefc6b656eab4e67634627e964ee
dc <00danzy@gmail.com>
2024-01-02 12:59:28 +0100

    feat: add constant error messages (10)

---
+++ src/app/core/constants/error-messages.ts
@@ -0,0 +1,16 @@
+const doesNotExist = ' does not exist';
+
+const errorMessages = {
+  usernameDoesNotExist: 'Username' + doesNotExist,
+  userDoesNotExist: 'User' + doesNotExist,
+  incorrectPassword: 'Password is incorrect',
+  passwordNewAndConfirmNoMatch: 'New password and confirm password do not match',
+  passwordRequired: 'Password is required',
+  passwordNewSameAsCurrent: 'The new password is the same as the current one',
+  emailNotRegistered: 'This email is not signed up on this platform',
+  usernameOrEmailRequired: 'Username or email must be provided',
+  notFound: 'not found',
+  invalidEmail: "Please enter a valid email",
+}
+
+export default errorMessages;
