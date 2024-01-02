c76e6e5cb2f59ed82b21951da51cd420e1cdf66e
dc <00danzy@gmail.com>
2024-01-02 12:51:18 +0100

    feat: add new routers
    auth, user, country, state, password

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,11 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { profileRouter } from "./routers/profile";
 import { schoolRouter } from "./routers/school";
 import { awsRouter } from "./routers/aws";
+import { authRouter } from "./routers/auth";
+import { userRouter } from "./routers/user";
+import { countryRouter } from "./routers/country";
+import { stateRouter } from "./routers/state";
+import { passwordRouter } from "./routers/password";
 
 /**
  * This is the primary router for your server.
@@ -9,9 +13,13 @@ import { awsRouter } from "./routers/aws";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  profile: profileRouter,
   school: schoolRouter,
   aws: awsRouter,
+  auth: authRouter,
+  user: userRouter,
+  country: countryRouter,
+  state: stateRouter,
+  password: passwordRouter,
 });
 
 // export type definition of API
