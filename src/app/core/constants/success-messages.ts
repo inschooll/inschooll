25efa052cac20fa2413179103e14ec8a95501508
dc <00danzy@gmail.com>
2024-01-02 12:55:55 +0100

    feat: add constant success messages (3)

---
+++ src/app/core/constants/success-messages.ts
@@ -0,0 +1,7 @@
+const successMessages = {
+  loginSuccessful: 'Login was successful.',
+  passwordChanged: 'Password changed successfully.',
+  resetPasswordEmailSent: "If your email address exists, you will receive a password reset link at your email address in a few seconds or minutes.",
+}
+
+export default successMessages;
