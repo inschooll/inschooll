8cc95d130df32d928feac6971d32092a86f7144a
dc <00danzy@gmail.com>
2024-01-02 12:55:16 +0100

    feat: create function that returns object used in sending mails using sendgrid

---
+++ src/app/utils/sendgrid.ts
@@ -0,0 +1,19 @@
+import sgMail from "@sendgrid/mail";
+import constants from "../core/constants/constants";
+import { env } from "~/env.mjs";
+
+sgMail.setApiKey(env.SENDGRID_API_KEY);
+
+interface mailMsgProp { to: string | string[]; subject: string, html: string; };
+
+export function mailMsg({ to, subject, html }: mailMsgProp) {
+  return {
+    to,
+    from: {
+      name: constants.appName,
+      email: env.SENDGRID_EMAIL,
+    },
+    subject,
+    html,    
+  };
+}
