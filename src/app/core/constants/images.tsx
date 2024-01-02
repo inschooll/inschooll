c06a9fb7682b51d531c0c97b76b2322d3ec651b1
dc <00danzy@gmail.com>
2024-01-02 12:58:01 +0100

    feat: add countryFlag helper function
    helps return a string path to where a particular countries flag is in our next.js project repo

--- src/app/core/constants/images.tsx
+++ src/app/core/constants/images.tsx
@@ -1,24 +1,26 @@
-const favicon = 'favicon.png';
-const logo = '/logo/logo.svg';
-const logoFull = '/logo/logo-full.svg';
-const brainPink = '/logo/brain-pink.svg';
-const bookStack = '/logo/book-stack.svg';
-const mathsCalculations = '/images/maths-calculations.svg';
-const mathsCalculation = '/images/maths-calculation.svg';
-const doubleArrowUp = '/icons/double-arrow-up.svg';
-const menu = '/icons/menu.svg';
-const cancel = '/icons/cancel.svg';
-const googleLogo = '/icons/google-logo.svg';
-const nigeriaFlag = '/icons/countries/nigeria.svg';
-const downArrow = '/icons/down-arrow.svg';
-const schoolLogo = '/images/schools/bingham.png';
-const maleAvatarDefault = '/icons/male-avatar-default.svg';
-const femaleAvatarDefault = '/icons/female-avatar-default.svg';
+const favicon = "favicon.png";
+const logo = "/logo/logo.svg";
+const logoFull = "/logo/logo-full.svg";
+const brainPink = "/logo/brain-pink.svg";
+const bookStack = "/logo/book-stack.svg";
+const mathsCalculations = "/images/maths-calculations.svg";
+const mathsCalculation = "/images/maths-calculation.svg";
+const doubleArrowUp = "/icons/double-arrow-up.svg";
+const menu = "/icons/menu.svg";
+const cancel = "/icons/cancel.svg";
+const googleLogo = "/icons/google-logo.svg";
+const nigeriaFlag = "/icons/countries/nigeria.svg";
+const downArrow = "/icons/down-arrow.svg";
+const schoolLogo = "/images/schools/bingham.png";
+const maleAvatarDefault = "/icons/male-avatar-default.svg";
+const femaleAvatarDefault = "/icons/female-avatar-default.svg";
+const countryFlag = (name: string) =>  // e.g "United States" -> '/icons/country-flags/united-states.svg'
+  `/icons/country-flags/${name.toLowerCase().replace(" ", "-")}.svg`;
 
 // lottie
-const spinner = '/images/lottie/spinner.json'
+const spinner = "/images/lottie/spinner.json";
 
-const random1 = '/images/random-1.jpg';
+const random1 = "/images/random-1.jpg";
 
 export default {
   favicon,
@@ -37,6 +39,7 @@ export default {
   schoolLogo,
   random1,
   spinner,
+  countryFlag,
   maleAvatarDefault,
   femaleAvatarDefault,
 } as const;
