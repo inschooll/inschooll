85aaf6e21201d19685a488dd97163723fb5315e5
dc <00danzy@gmail.com>
2024-01-04 18:47:07 +0100

    feat: add authorization
    ### changes
    
    - applicable to all routes except landing page, login, sign up reset password pages
    - ensures authorized users that visit login or sign up pages are redirected back into the application

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { jwtVerify } from 'jose';
+import { env } from '~/env.mjs';
+
+export interface UserJWTPayload {
+  jti: string;
+  iat: number;
+  exp: number;
+  userId?: string;
+}
+
+export const verifyJWT = async (token: string) => {
+  try {
+    const verified = await jwtVerify(token, new TextEncoder().encode(env.SECRET_KEY));
+    return verified.payload as UserJWTPayload;
+  } catch (error) {
+    throw new Error('token has expired')
+  } 
+}
---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import constants from './app/core/constants/constants';
+import { verifyJWT } from './lib/auth';
+import links from './app/core/constants/links';
+
+export async function middleware(req: NextRequest) {
+  const route = req.nextUrl.pathname;
+  const publicRoutes: string[] = [links.landingPage, links.login, links.signup, links.resetPassword];  // home, login, signup, reset password
+  // check if route this request is trying to visit is towards a public route
+  const isPublicRoute = publicRoutes.includes(route);
+  
+  // grab cookies
+  const authToken = req.cookies.get(constants.tokenName)?.value;
+
+  // verify jwt. TODO: test the result when theres not token
+  const payload = await verifyJWT(authToken!).catch((err) => console.log(err));
+  
+  console.log('🎯route:', route);
+  console.log('🦴payload:', payload);
+
+  // uf a user visits a public route without being authenticated
+  if (isPublicRoute && !payload) return;
+
+  // TODO: if user is authenticated and visits a public route, we should 
+  // redirect them to a page where we can ask them which school they belong 
+  // (out of all the school's they have a role in) to, but if they happen to not 
+  // belong to any school then we must redirect them to the purpose screen
+  if (isPublicRoute && payload) {
+    // Get roles. if any redirect request to a page that aks to know which school 
+    // the user would like to access, 
+    // and if no roles for the user was found, redirect the user to the purpose screen 
+    return NextResponse.redirect(new URL(links.purpose, req.url));
+  }
+
+  // if unauthorized
+  if (!isPublicRoute && !payload) {
+    return NextResponse.redirect(new URL(links.login, req.url));
+  }
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|images|favicon.ico).*)',
+};
