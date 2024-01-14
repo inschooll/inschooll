9cc6c1c34d88ba4654041405eee4540b44c4f533
dc <00danzy@gmail.com>
2024-01-14 18:10:48 +0100

    feat: add schoolCreated message

--- src/app/core/constants/success-messages.ts
+++ src/app/core/constants/success-messages.ts
@@ -2,6 +2,7 @@ const successMessages = {
   loginSuccessful: 'Login was successful.',
   passwordChanged: 'Password changed successfully.',
   resetPasswordEmailSent: "If your email address exists, you will receive a password reset link at your email address in a few seconds or minutes.",
+  schoolCreated: "'School was successfully created!."
 }
 
 export default successMessages;
