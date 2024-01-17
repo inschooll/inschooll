32421886ac7e86287cbbaf951b229eaf16ec4626
dc <00danzy@gmail.com>
2024-01-17 01:42:35 +0100

    feat: improve navbar selection

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -4,9 +4,6 @@ import { useState } from "react";
 type navItemType = {icon?: string, text: string}[];
 
 export default function Navbar({navItems, defaultSelected=0, updateSelected, ulClassName=''} : {navItems: navItemType, defaultSelected?: number, ulClassName?: string, updateSelected: (index: number) => void}) {
-  const selectedStyle = "border-b-[3px] border-cc-content-main/70";
-  const hoverStyle = "hover:border-b-[3px] hover:border-cc-content-main/40";
-
   const [selectedIndex, setSelectedIndex] = useState(defaultSelected);
 
   const update = (index: number) => {
@@ -16,16 +13,19 @@ export default function Navbar({navItems, defaultSelected=0, updateSelected, ulC
 
   function NavItem({index, item} : {index: number, item: navItemType[0]}) {
     return (
-      <div className={selectedIndex === index ? selectedStyle : hoverStyle}>
+      <div className='relative'>
+        {selectedIndex === index && (<span className="absolute w-full h-[.2rem] bg-cc-primary bottom-0 rounded-t" />)}
         {item?.icon}
-        <li className={`cursor-pointer px-1 py-[10px] font-medium ${selectedIndex === index ? "text-cc-content-sub" : "text-cc-content-sub/50"}`} onClick={() => update(index)}>{item.text}</li>
+        <div className={`cursor-pointer font-medium ${selectedIndex === index ? "text-cc-content-sub" : "text-cc-content-sub/50"}`}>
+          <li className={`hover:app-hover px-2 py-0.5 mb-1.5 rounded-md`} onClick={() => update(index)}>{item.text}</li>
+        </div>
       </div>
     );
   }
 
   return (
     <nav className="block border-b border-cc-border-main">
-      <ul className={`flex justify-stretch gap-4 ${ulClassName}`}>
+      <ul className={`flex justify-stretch gap-2 ${ulClassName}`}>
         {navItems?.map((item, index) => (<NavItem index={index} item={item} key={index} />))}
       </ul>
     </nav>
