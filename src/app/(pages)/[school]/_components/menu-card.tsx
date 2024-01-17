9745cb072747c3b23d91eb13df664728abbc86e2
dc <00danzy@gmail.com>
2024-01-17 01:45:26 +0100

    feat: introduce new styles that improves the look and feel

--- src/app/(pages)/[school]/_components/menu-card.tsx
+++ src/app/(pages)/[school]/_components/menu-card.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import links from "~/app/core/constants/links";
+import { cn } from "~/lib/utils";
 
 type ComponentProps = {
   title: string;
@@ -21,16 +21,16 @@ export default function MenuCard({
   const pathname = usePathname();
   const isSelected = pathname.includes(navigateTo);
 
-  const textStyle = isSelected ? "text-cc-content-main/80" : "text-cc-content-main/60";
-  const iconStyle = isSelected ? "text-cc-content-main" : "text-cc-content-main/50";
-  const bgStyle = isSelected ? "bg-cc-border-main/50" : "";
+  const textStyle = isSelected ? "text-cc-primary" : "text-cc-content-main/60";
+  const iconStyle = isSelected ? "text-cc-primary" : "text-cc-content-main/50";
+  const bgStyle = isSelected ? "bg-cc-sidebar-bg hover:bg-cc-sidebar-bg-hover" : "";
 
   // add props to icon
   const IconWithProps = React.Children.map(icon, child => {
     if (React.isValidElement(child)){
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      return React.cloneElement(child,{className: `${iconStyle}`})
+      return React.cloneElement(child,{className: `${iconStyle}`});
     }
     return child;
   })
@@ -38,10 +38,12 @@ export default function MenuCard({
   return (
     <Link href={navigateTo}>
       <div
-        className={`flex cursor-pointer items-center gap-3 rounded-md px-3 py-2 app-hover ${bgStyle}`}
+        className={cn("relative flex cursor-pointer text-cc-primary items-center gap-3 rounded-md px-3 py-2 hover:bg-cc-sidebar-bg-hover transition-all duration-200", bgStyle)}
       >
+        {!!isSelected && (<span className="absolute left-0 h-4 w-[.28rem] bg-cc-primary rounded-r-md" />) }
+
         {IconWithProps}
-        <p className={`font-medium capitalize ${textStyle}`}>{title}</p>
+        <p className={cn("font-medium capitalize", textStyle)}>{title}</p>
       </div>
     </Link>
   );
