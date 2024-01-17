822cadc3c75c4f0bebb76a60ba8121e6d7c30641
dc <00danzy@gmail.com>
2024-01-17 01:45:40 +0100

    refactor: to allow breadcrumbs have accurate padding

--- src/app/(pages)/[school]/_components/breadcrumbs-and-title.tsx
+++ src/app/(pages)/[school]/_components/breadcrumbs-and-title.tsx
@@ -4,14 +4,18 @@ import BreadCrumbs from "./breadcrumbs";
 export default function BreadCrumbsAndTitle({ title, breadCrumbData }: { title: string, breadCrumbData: Record<string, string>}) {
 
   return (
-    <div className="px-7">
+    <>
       {/* Bread crumbs */}
-      <BreadCrumbs patterns={breadCrumbData} />
-
-      {/* Title */}
-      <div className="mt-4">
-        <T4 weight="semibold">{title}</T4>
+      <div className="px-2">
+        <BreadCrumbs patterns={breadCrumbData} />
       </div>
-    </div>
+
+      <div className="px-7">
+        {/* Title */}
+        <div className="mt-4">
+          <T4 weight="semibold">{title}</T4>
+        </div>
+      </div>
+    </>
   );
 }
