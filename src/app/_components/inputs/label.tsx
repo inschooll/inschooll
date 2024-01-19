6613c5465cc6e23cb3fb94043adc0bb747d4eae1
dc <00danzy@gmail.com>
2024-01-19 09:53:35 +0100

    refactor: move files

--- src/app/_components/inputs/label.tsx
+++
@@ -1,8 +0,0 @@
-export default function Label({className, value, labelFor, isRequired} : {className?: string, value: string, labelFor?: string, isRequired: boolean}) {
-  return (
-    <label htmlFor={labelFor} className={`text-xs font-medium text-cc-content-main/80 ${className}`}>
-      {value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()}
-      {isRequired && <span className="text-red-500"> *</span>}
-    </label>
-  );
-}
