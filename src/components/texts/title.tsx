4c9fe7a62e6471a8a3deea61b7e93083fdf10582
dc <00danzy@gmail.com>
2024-01-27 09:23:07 +0100

    feat: introduce cn utility function and pass accept className prop

--- src/components/texts/title.tsx
+++ src/components/texts/title.tsx
@@ -1,36 +1,40 @@
-interface TProps {
+import { cn } from "~/lib/utils";
+
+type TProps = {
   children: React.ReactNode, 
-  color?: string,
-  weight?: "regular" | "medium" | "semibold" | "bold"
+  className?: string,
+  // TODO: Remove color and weight since they are now deprecated
+  color?: string, 
+  weight?: "regular" | "medium" | "semibold" | "bold",
 }
 
-export function T1({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T1({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-5xl ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-5xl text-cc-content-sub", className)}>{children}</h4>
   );
 }
-export function T2({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T2({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-4xl ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-4xl text-cc-content-sub", className)}>{children}</h4>
   );
 }
-export function T3({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T3({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-3xl ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-3xl text-cc-content-sub", className)}>{children}</h4>
   );
 }
-export function T4({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T4({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-[26px] ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-[26px] text-cc-content-sub", className)}>{children}</h4>
   );
 }
-export function T5({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T5({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-xl ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-xl text-cc-content-sub", className)}>{children}</h4>
   );
 }
-export function T6({children, weight="semibold", color="text-cc-content-sub"} : TProps) {
+export function T6({children, className} : TProps) {
   return (
-    <h4 className={`font-${weight} text-lg ${color}`}>{children}</h4>
+    <h4 className={cn("font-semibold text-lg text-cc-content-sub", className)}>{children}</h4>
   );
 }
