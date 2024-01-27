c3b16e06975c4c6b584360a0c1bda0d13f69988b
dc <00danzy@gmail.com>
2024-01-27 09:26:07 +0100

    feat: improve code

--- src/app/(pages)/(main)/student-affairs/sdc/cases/[id]/_components/body.tsx
+++ src/app/(pages)/(main)/student-affairs/sdc/cases/[id]/_components/body.tsx
@@ -18,7 +18,7 @@ export default function CaseBody() {
 
       {selectedIndex === 0 && <Body />}
       {selectedIndex === 1 && <Comments />}
-      {selectedIndex === 2 && <Verdict />}
+      {selectedIndex === 2 && <Verdict />};
     </>
   );
 } 
