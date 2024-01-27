327203985b6cfff1ceeed847a401ae963015de01
dc <00danzy@gmail.com>
2024-01-27 09:26:52 +0100

    styles: improve

--- src/app/(pages)/(main)/_components/breadcrumbs.tsx
+++ src/app/(pages)/(main)/_components/breadcrumbs.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
+import { cn } from "~/lib/utils";
 
-export default function BreadCrumbs({patterns}: {patterns: Record<string, string>}) {
+type BreadCrumbsProps = {patterns: Record<string, string>, className?: string,};
+
+export default function BreadCrumbs({patterns, className}: BreadCrumbsProps) {
   // e.g dashboard > lessons > assignments > submission
   const pages = Object.keys(patterns);
 
   return (
-    <div className="flex h-5">
+    <div className={cn("flex p-2 text-cc-content-sub text-sm", className)}>
       {pages.map((page, i) => (
-        <div key={page} className="flex text-cc-content-sub">
+        <div key={page} className="flex">
           <Link href={patterns[page]!}>
             <div className="app-hover px-2.5 mx-1 py-0.5 rounded-md">
-              <p className="text-sm">
+              <p>
                 {page}
               </p>
             </div>
           </Link>
 
-          {i < pages.length-1 && <span className=" text-cc-content-sub/50">/</span>}
+          {i < pages.length-1 && <span className="text-base text-cc-content-sub/50">/</span>}
         </div>
       ))}
     </div>
