46dc23a879f1af4f611fe54900bec6c594226ebd
dc <00danzy@gmail.com>
2024-01-29 00:35:55 +0100

    feat: add BackOfficeSidebar

--- src/app/(pages)/back-office/layout.tsx
+++ src/app/(pages)/back-office/layout.tsx
@@ -1,48 +1,24 @@
-import { LuHeartHandshake, LuSchool } from "react-icons/lu";
-import { MdOutlineSpaceDashboard } from "react-icons/md";
-import links from "~/app/core/constants/links";
-import { BackOfficeHeader } from "./_components/back-office-header";
-import SidebarButton from "~/components/sidebar-button";
+import BackOfficeHeader from "./_components/header";
+import BackOfficeSidebar from "./_components/sidebar";
 
 export default function Layout({children} : {children: React.ReactNode}) {
   return (
     <div>
       <BackOfficeHeader />
-      <div className="">
-        {children}
-      </div>
-    </div>
-  )
-}
+      <div className="pt-5 flex justify-center min-h-screen">
+        <div className="flex-1 mx-5 xl:-translate-x-16 md:max-w-[68rem] border-r border-red-500">
+          <div className="flex">
+            {/* Left navbar */}
+            <BackOfficeSidebar className="hidden lg:block" />
 
-const BackOfficeSidebar = () => {
-  return (
-    <nav>
-      <div className="px-4">
-        <div>
-          <div className="mt-2 flex flex-col gap-2">
-            {/* dashboard */}
-            <SidebarButton 
-              title="Dashboard"
-              icon={<MdOutlineSpaceDashboard className="" size={20} />}
-              navigateTo={links.dashboard}
-              />
-            {/* school */}
-            <SidebarButton 
-              title="School"
-              icon={<LuSchool className="" size={20} />}
-              navigateTo={links.school}
-              />
-            {/* sdc */}
-            <SidebarButton 
-              title="Student Affairs"
-              icon={<LuHeartHandshake className="" size={20} />}
-              navigateTo={links.studentAffairs}
-            />
+            <div className="flex-grow md:px-10">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
+
 
