7bdda441f6093cb25eadb179b30f5810d9566777
dc <00danzy@gmail.com>
2024-01-29 00:27:15 +0100

    feat: add selection via useMemo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,53 +1,65 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 import { cn } from "~/lib/utils";
 
-type navItemType = {icon?: string; text: string, href: string }[];
-
-/**
- * navItems[i].href and selectedTab are compared to know which navItem is selected.
- * ex
- * navItems.href = '/q=about'
- * selectedTab = 'about'
- * 
- */
+type navItemType = { icon?: string; text: string; href: string }[];
 type NavbarProps = {
-  navItems: {icon?: string; text: string, href: string }[];
+  navItems: { icon?: string; text: string; href: string }[];
   className?: string;
   ulClassName?: string;
-  selectedTab: string | undefined;  // expect e.g 'home' or 'about' or undefined (this should mean we are at the 1st navItem)
 };
 
 export default function Navbar(props: NavbarProps) {
-  const NavItem = ({ index, item }: { index: number; item: navItemType[0] }) => {
-    const href = item.href.substring(3,); // ?q=about -> about
-    const isSelected = (props.selectedTab === undefined && index === 0) || (props.selectedTab === href);
+  const pathname = usePathname();
+  const selectedIndex = useMemo(() => {
+    let index = 0;
+    // set selected sidebar item index
+    props.navItems.map((item, i) => {
+      if (pathname.startsWith(item.href)) index = i;
+    });
 
-    return (
-      <Link href={item.href} className="relative">
-          {isSelected && (
-            <span className="absolute bottom-0 h-[.2rem] w-full rounded-t bg-cc-primary" />
-          )}
-          {item?.icon}
-          <div
-            className={`cursor-pointer font-medium ${isSelected ? "text-cc-content-sub" : "text-cc-content-sub/50"}`}
-          >
-            <li
-              className={`hover:app-hover mb-1.5 rounded-md px-2 py-0.5`}
-            >
-              {item.text}
-            </li>
-          </div>
-      </Link>
-    );
-  }
+    return index;
+  }, [pathname, props.navItems]);
 
   return (
-    <nav className={cn("block border-b border-cc-border-main", props.className)}>
+    <nav
+      className={cn("block border-b border-cc-border-main", props.className)}
+    >
       <ul className={cn("flex justify-stretch gap-4", props.ulClassName)}>
         {props.navItems?.map((item, index) => (
-          <NavItem index={index} item={item} key={index} />
+          <NavItem
+            item={item}
+            key={index}
+            isSelected={selectedIndex === index}
+          />
         ))}
       </ul>
     </nav>
   );
 }
+
+const NavItem = ({
+  item,
+  isSelected,
+}: {
+  item: navItemType[0];
+  isSelected: boolean;
+}) => {
+  return (
+    <Link href={item.href} className="relative">
+      {isSelected && (
+        <span className="absolute bottom-0 h-[.2rem] w-full rounded-t bg-cc-primary" />
+      )}
+      {item?.icon}
+      <div
+        className={`cursor-pointer font-medium ${isSelected ? "text-cc-content-sub" : "text-cc-content-sub/50"}`}
+      >
+        <li className={`hover:app-hover mb-1.5 rounded-md px-2 py-0.5`}>
+          {item.text}
+        </li>
+      </div>
+    </Link>
+  );
+};
