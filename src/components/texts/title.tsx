77468a02925cf53ead67400f50c7bbe6d91442d4
dc <00danzy@gmail.com>
2024-01-29 00:24:19 +0100

    feat: make components web responsive

--- src/components/texts/title.tsx
+++ src/components/texts/title.tsx
@@ -10,31 +10,31 @@ type TProps = {
 
 export function T1({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-5xl text-cc-content-sub", className)}>{children}</h4>
+    <h1 className={cn("font-semibold text-4xl md:text-5xl text-cc-content-sub", className)}>{children}</h1>
   );
 }
 export function T2({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-4xl text-cc-content-sub", className)}>{children}</h4>
+    <h2 className={cn("font-semibold text-3xl md:text-4xl text-cc-content-sub", className)}>{children}</h2>
   );
 }
 export function T3({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-3xl text-cc-content-sub", className)}>{children}</h4>
+    <h3 className={cn("font-semibold text-2xl md:text-3xl text-cc-content-sub", className)}>{children}</h3>
   );
 }
 export function T4({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-[26px] text-cc-content-sub", className)}>{children}</h4>
+    <h4 className={cn("font-semibold text-xl md:text-[26px] text-cc-content-sub", className)}>{children}</h4>
   );
 }
 export function T5({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-xl text-cc-content-sub", className)}>{children}</h4>
+    <h5 className={cn("font-semibold text-lg md:text-xl text-cc-content-sub", className)}>{children}</h5>
   );
 }
 export function T6({children, className} : TProps) {
   return (
-    <h4 className={cn("font-semibold text-lg text-cc-content-sub", className)}>{children}</h4>
+    <h6 className={cn("font-semibold text-base md:text-lg text-cc-content-sub", className)}>{children}</h6>
   );
 }
