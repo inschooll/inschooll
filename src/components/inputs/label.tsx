dc8cb79f4734ef493efcc1a16f9ff33c8bd0b52d
dc <00danzy@gmail.com>
2024-01-29 00:24:50 +0100

    feat: increase label font size

--- src/components/inputs/label.tsx
+++ src/components/inputs/label.tsx
@@ -1,6 +1,6 @@
 export default function Label({className, value, labelFor, isRequired} : {className?: string, value: string, labelFor?: string, isRequired: boolean}) {
   return (
-    <label htmlFor={labelFor} className={`text-xs font-medium text-cc-content-main/80 ${className}`}>
+    <label htmlFor={labelFor} className={`text-sm font-medium text-cc-content-main/80 ${className}`}>
       {value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()}
       {isRequired && <span className="text-red-500"> *</span>}
     </label>
