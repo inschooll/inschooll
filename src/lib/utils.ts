d45f0ca56d1c066fdfea52d7e555b2567991c38a
dc <00danzy@gmail.com>
2024-02-06 20:15:07 +0100

    feat: add getNumberPrefix and getLevelTitle utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,40 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getLevelTitle = (value: number) => {
+  switch (value) {
+    case 1:
+      return "100 level";
+    case 2:
+      return "200 level";
+    case 3:
+      return "300 level";
+    case 4:
+      return "400 level";
+    case 5:
+      return "500 level";
+    case 6:
+      return "600 level";
+    case 7:
+      return "700 level";
+    case 8:
+      return "800 level";
+    default:
+      return "unknown level";
+  }
+};
+
+export const getNumberPrefix = (value: number) => {
+
+  switch (value) {
+    case 1:
+      return '1st';
+    case 2:
+      return '2nd';
+    case 3:
+      return '3rd';
+    default:
+      return value + 'th';
+  }
+}
