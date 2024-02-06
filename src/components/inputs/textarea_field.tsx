64f060d59a4104f031c07ff5d525842aa37711c8
dc <00danzy@gmail.com>
2024-02-06 20:09:34 +0100

    feat: improve component and use InputLabel for displaying of description

--- src/components/inputs/textarea_field.tsx
+++ src/components/inputs/textarea_field.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { type TextareaHTMLAttributes, type DetailedHTMLProps } from "react";
 import { InputDescription, InputErrorMessage, InputLabel } from "./input";
 import { useFormContext } from "react-hook-form";
@@ -14,15 +15,12 @@ export default function TextareaField({...props}: TextareaProps) {
   return (
     <div>
       {/* label */}
-      {props.label && (<InputLabel label={props.label} minLength={props.minLength} maxLength={props.maxLength} required={props.required} />)}
-
-      {/* description */}
-      {!!props.description && (<InputDescription description={props.description} />)}
+      {props.label && (<InputLabel label={props.label} description={props.description} minLength={props.minLength} maxLength={props.maxLength} required={props.required} />)}
 
       <textarea
         rows={props.rows ?? 8}
         className={
-          "focus:border-1 resize-none w-full rounded border-2 bg-cc-input-bg px-2 text-cc-input-text outline-none transition-colors duration-200 placeholder:font-normal placeholder:text-cc-content-main/20 border-cc-border-main focus:border-cc-primary-main"
+          "focus:border-1 resize-none w-full rounded border-2 bg-cc-input-bg px-2 py-1.5 text-cc-input-text outline-none transition-colors duration-200 placeholder:font-normal placeholder:text-cc-content-main/20 border-cc-border-main focus:border-cc-primary-main"
         }
         {...methods?.register(props.name!) }
         {...props}
