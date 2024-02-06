6671b63e9f1a4aedd283781e7e2273f681fea3aa
dc <00danzy@gmail.com>
2024-02-06 20:17:48 +0100

    feat: implement initial design

--- src/app/(pages)/back-office/departments/page.tsx
+++ src/app/(pages)/back-office/departments/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import links from "~/app/core/constants/links";
+import Input from "~/components/inputs/input";
+import { SectionTitle } from "../_components/section-title";
+import { buttonVariants } from "~/components/ui/button";
+import { MdAdd } from "react-icons/md";
 
 export default function Page() {
   return (
-    <div>Departments</div>
-  )
+    <div className="flex flex-col gap-5">
+      <SectionTitle title="Departments" variant="lg" />
+
+      {/* Search <-> Add degree and co */}
+      <div className="mt-1 flex justify-between">
+        <Input />
+
+        <div className="flex gap-2">
+          <Link
+            href={links.backoffice.departments.create}
+            className={buttonVariants({ variant: "default" })}
+          >
+            <MdAdd size={16} />
+            <p className="pl-0.5">Add department</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
