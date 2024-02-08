29cf37bbb4cbcdbfb9f76cd608b39d38d31640f6
dc <00danzy@gmail.com>
2024-02-08 23:04:47 +0100

    first commit

---
+++ src/components/ui/custom/select-combo.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { cn } from "~/lib/utils";
+
+export type SelectComboFrameworksProps = {
+  avatar?: string;
+  value: string;
+  label: string;
+}[];
+
+export type SelectComboProps = {
+  defaultValue?: string;
+  frameworks: SelectComboFrameworksProps;
+  buttonClassName?: string;
+  onChange?: (value: string) => void;
+};
+
+export default function SelectCombo({
+  defaultValue = "Select ...",
+  frameworks,
+  buttonClassName,
+  onChange,
+}: SelectComboProps) {
+  const [selected, setSelected] = useState<
+    SelectComboFrameworksProps[0] | null
+  >(null);
+
+  return (
+    <Select
+      onValueChange={(value) => {
+        if (value === selected?.value) return setSelected(null);
+
+        const newSelected = frameworks.find(
+          (framework) => framework.value === value,
+        );
+        setSelected(newSelected ?? null);
+        onChange && onChange(value);
+      }}
+    >
+      <SelectTrigger className={cn("w-[180px]", buttonClassName)}>
+        <SelectValue placeholder={defaultValue} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {frameworks.map((framework) => (
+            <SelectItem value={framework.label} key={framework.value}>
+              {framework.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
