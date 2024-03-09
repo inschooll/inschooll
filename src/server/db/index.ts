11c3e0d056cb8e7a4b2aa585b855023fc14cdec8
dc <00danzy@gmail.com>
2024-03-09 23:44:22 +0000

    feat: replace planetscale with turso

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,13 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 
-import { env } from "~/env.mjs";
+import { env } from "~/env";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema }
-);
+export const prodDBCredentials = { url: env.TURSO_DATABASE_URL, authToken: env.TURSO_AUTH_TOKEN };
+export const devDBCredentials = { url: "http://127.0.0.1:8080" }
+
+const client = createClient(env.NODE_ENV === 'production' ? prodDBCredentials : devDBCredentials);
+
+
+export const db = drizzle(client, { schema });
