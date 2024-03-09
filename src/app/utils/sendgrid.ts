5143e6681133dc400854c8cfea94aa267164c4bc
dc <00danzy@gmail.com>
2024-03-09 23:49:38 +0000

    fix: import

--- src/app/utils/sendgrid.ts
+++ src/app/utils/sendgrid.ts
@@ -1,6 +1,6 @@
 import sgMail from "@sendgrid/mail";
 import constants from "../core/constants/constants";
-import { env } from "~/env.mjs";
+import { env } from "~/env.js";
 
 sgMail.setApiKey(env.SENDGRID_API_KEY);
 
