041b30a7b78ac952efddd2a3c9179afa1ca6ef0e
dc <00danzy@gmail.com>
2024-03-09 23:56:24 +0000

    feat: improve header

--- src/app/(pages)/back-office/_components/header.tsx
+++ src/app/(pages)/back-office/_components/header.tsx
@@ -1,50 +1,57 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 import { HiMenu } from "react-icons/hi";
-import images from '~/app/core/constants/images';
-import links from '~/app/core/constants/links';
-import AppLogo from '~/components/app_logo';
-import Navbar from '~/components/navbar';
+import images from "~/app/core/constants/images";
+import links from "~/app/core/constants/links";
+import AppLogo from "~/components/app_logo";
+import Navbar from "~/components/navbar";
 import { Button } from "~/components/ui/button";
 
 export default function BackOfficeHeader() {
-  
-  const navItems = [
-    { text: "/", href: links.backoffice.main },
-  ];
-  
+  const navItems = [{ text: "/", href: links.backoffice.main }];
+
   return (
-    <header className='border-b border-cc-border bg-cc-background-sub'>
-      <div className="flex items-center gap-4 p-4">
-        {/* Menu Button */}
-        <Button variant={'secondary'} size={'icon'} className='rounded-lg text-cc-content/50 hover:text-cc-content hover:bg-transparent'>
-          <HiMenu size={20}/>
-        </Button>
-
-        {/* Logo */}
-        <Link href={links.dashboard}>
-          <AppLogo size='lg' showAppName={false} />
-        </Link>
-
-        {/* School btn */}
-        <Button variant={'secondary'} className='ml-5 rounded-lg'>
-          <div className="flex items-center gap-2">
-            <Image
-              src={images.logos.bingham}
-              alt='school logo'
-              width={17}
-              height={17}
-            />
-            <p>Bingham University</p>
-          </div>
-        </Button>
-      </div>
+    <header className="border-b border-cc-border bg-cc-background-sub">
+      <MenuActivatorAndLogo />
 
       {/* Navbar */}
-      <div className="pt-1 px-4">
-        <Navbar navItems={navItems} className='border-0'/>
+      <div className="px-4 pt-1">
+        <Navbar navItems={navItems} className="border-0" />
       </div>
     </header>
+  );
+}
+
+function MenuActivatorAndLogo() {
+  return (
+    <div className="flex items-center gap-4 p-4">
+      {/* Menu Button */}
+      <Button
+        variant={"secondary"}
+        size={"icon"}
+        className="rounded-lg text-cc-content/50 hover:bg-transparent hover:text-cc-content"
+      >
+        <HiMenu size={20} />
+      </Button>
+
+      {/* Logo */}
+      <Link href={links.dashboard}>
+        <AppLogo size="lg" showAppName={false} />
+      </Link>
+
+      {/* School btn */}
+      <Button variant={"secondary"} className="ml-5 rounded-lg">
+        <div className="flex items-center gap-2">
+          <Image
+            src={images.logos.bingham}
+            alt="school logo"
+            width={17}
+            height={17}
+          />
+          <p>Bingham University</p>
+        </div>
+      </Button>
+    </div>
   );
 }
