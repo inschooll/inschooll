fb0d3b4977388b381a85131cd5aae4a1ee0039a7
dc <00danzy@gmail.com>
2024-03-09 23:58:01 +0000

    feat: add sidebar activator button and breadcrumbs

---
+++ src/app/(pages)/(main)/dashboard/components/dashboard-navbar.tsx
@@ -0,0 +1,38 @@
+'use client';
+import Link from "next/link";
+import links from "~/app/core/constants/links";
+import AppLogo from "~/components/app_logo";
+import LeftSidebarActivatorButton from "~/components/buttons/left-sidebar-activator-button";
+import { cn } from "~/lib/utils";
+import { useLeftSidebarStore } from "~/store";
+import BreadCrumbs from "../../_components/breadcrumbs";
+
+const breadCrumbData = {'Dashboard': links.dashboard};
+
+export default function DashboardNavbar({}) {
+  const isActive = useLeftSidebarStore(state => state.isActive);
+  const isMobileActive = useLeftSidebarStore(state => state.isMobileActive);
+
+
+  return (
+    <div className="flex gap-2 items-center">
+      {!isActive && <SidebarActivatorAndAppLogo className="hidden lg:flex" />}
+      {!isMobileActive && <SidebarActivatorAndAppLogo className="flex lg:hidden" />}
+      <BreadCrumbs patterns={breadCrumbData} />
+
+    </div>
+  );
+}
+
+function SidebarActivatorAndAppLogo({className} : {className: string}) {
+  return (
+    <div className={cn("flex items-center gap-1 py-2", className)}>
+      <LeftSidebarActivatorButton />
+
+      {/* Logo */}
+      <Link href={links.dashboard}>
+        <AppLogo size="sm" showAppName={false} />
+      </Link>
+    </div>
+  );
+}
