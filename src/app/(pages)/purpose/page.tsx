e2cc02d27d10b9862fab5c98e882a97adf16ae58
dc <00danzy@gmail.com>
2024-03-09 23:53:58 +0000

    fix: improve text

--- src/app/(pages)/purpose/page.tsx
+++ src/app/(pages)/purpose/page.tsx
@@ -8,12 +8,11 @@ export default function Purpose() {
     <>
       <TopNavbar />
 
-      <main className="p-5 sm:p-0 lg:mx-20 h-screen">
+      <main className="p-5 sm:p-0 lg:mx-20 h-[80vh] group">
         <div className="flex flex-col justify-center items-center h-full">
-          <div className="sm:w-96 -translate-y-[20%] py-10">
-            <h1 className="text-center font-bold text-2xl sm:text-3xl">
-              Which option best describes you?
-            </h1>
+          <div className="flex flex-col justify-center gap-4 text-center sm:w-96 ">
+            <h1 className="font-bold text-3xl">Who are you?</h1>
+            <p className="text-cc-content/70">Pick any one of the options that best describes who you are 🤵</p>
 
             <PurposeBody />
 
