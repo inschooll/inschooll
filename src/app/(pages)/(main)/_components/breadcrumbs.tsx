1cfe59c9fd65f3093044a24e74f5199d18cbc653
dc <00danzy@gmail.com>
2024-03-10 23:56:57 +0100

    styles: improve border radius

--- src/app/(pages)/(main)/_components/breadcrumbs.tsx
+++ src/app/(pages)/(main)/_components/breadcrumbs.tsx
@@ -12,14 +12,14 @@ export default function BreadCrumbs({patterns, className}: BreadCrumbsProps) {
       {pages.map((page, i) => (
         <div key={page} className="flex">
           <Link href={patterns[page]!}>
-            <div className="app-hover px-2.5 mx-1 py-0.5 rounded-md">
+            <div className="app-hover px-2 py-0.5 rounded-sm">
               <p>
                 {page}
               </p>
             </div>
           </Link>
 
-          {i < pages.length-1 && <span className="text-base text-cc-content-sub/50">/</span>}
+          {i < pages.length-1 && <span className="text-base text-cc-content-sub/50 px-1">/</span>}
         </div>
       ))}
     </div>
