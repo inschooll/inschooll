ab76ec6a5881118d629e53cdd08149c0add332de
dc <00danzy@gmail.com>
2024-03-10 00:03:22 +0000

    feat: discard planetscale and implement turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
 import { type Config } from "drizzle-kit";
 
-import { env } from "~/env.mjs";
+import { env } from "~/env";
+import { devDBCredentials, prodDBCredentials } from "~/server/db";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  driver: "mysql2",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
+  out: "./migrations",
+  driver: "turso",
+  dbCredentials: env.NODE_ENV === 'production' ? prodDBCredentials : devDBCredentials,
   tablesFilter: ["inschooll_*"],
+  // Print all statement
+  verbose: true,
+  // Always ask for my permission
+  // strict: true,
 } satisfies Config;
