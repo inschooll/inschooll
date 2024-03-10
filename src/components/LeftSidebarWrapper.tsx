a3c3e89b19fe3577715c8e9738b74567e8a2b379
dc <00danzy@gmail.com>
2024-03-10 23:55:01 +0100

    feat: take new className prop

--- src/components/LeftSidebarWrapper.tsx
+++ src/components/LeftSidebarWrapper.tsx
@@ -3,7 +3,7 @@ import Sidebar from "~/app/(pages)/(main)/_components/sidebar";
 import { cn } from "~/lib/utils";
 import { useLeftSidebarStore } from "~/store";
 
-export default function LeftSidebarWrapper({}) {
+export default function LeftSidebarWrapper({sidebarClassName} : {sidebarClassName: string}) {
   const isActive = useLeftSidebarStore((state) => state.isActive);
   const isMobileActive = useLeftSidebarStore((state) => state.isMobileActive);
   const setMobileActive = useLeftSidebarStore((state) => state.setMobileActive);
@@ -12,13 +12,13 @@ export default function LeftSidebarWrapper({}) {
 
   return (
     <>
-      {isActive && <Sidebar className={cn("hidden lg:block")} />}
+      {isActive && <Sidebar className={cn("hidden lg:block", sidebarClassName)} />}
       {isMobileActive && (
         <>
           <div>
             <Sidebar
               className={cn(
-                "lg:hidden absolute left-1 top-1/2 z-10 block h-[99%] -translate-y-1/2 rounded-lg border border-cc-border shadow-md ",
+                "lg:hidden absolute left-1 top-1/2 z-10 block h-[99%] -translate-y-1/2 rounded-lg border border-cc-border shadow-md ", sidebarClassName
               )}
               onSidebarButtonClick={disableMobileSidebar}
             />
