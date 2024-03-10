72c5d0181607825367fdc80a6a0ae2bbd47e72a9
dc <00danzy@gmail.com>
2024-03-11 00:00:40 +0100

    fix: ensure page width == 100% regards the sidebars width 🐦🦜
    eg
    children pages width 100% should be 100%-sidebars width

--- src/app/(pages)/(main)/layout.tsx
+++ src/app/(pages)/(main)/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
 import React from "react";
 import LeftSidebarWrapper from "~/components/LeftSidebarWrapper";
 import StyleAdder from "./style-adder";
+import { useLeftSidebarStore } from "~/store";
+import { cn } from "~/lib/utils";
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -8,23 +11,23 @@ export interface LayoutProps {
 }
 
 export default function Layout({ children, params }: LayoutProps) {
-  console.log(params);
+  const isLeftSidebarActive = useLeftSidebarStore(state => state.isActive);
 
   return (
     <div className="h-[100vh]">
-      {/* This component helps add unique css properties to the body element when it mounts */}
+      {/* This component helps add unique css properties 
+      to the body element when it mounts */}
       <StyleAdder />
 
-      {/* <MainNavbar schoolName={params.school} /> */}
-
       <div className="flex h-full justify-stretch bg-cc-background-sub">
-        {/* Left navbar */}
-        <LeftSidebarWrapper />
+        {/* sidebar */}
+        <LeftSidebarWrapper sidebarClassName="w-64" />
 
-        <div className="flex-grow overflow-auto pb-2">{children}</div>
+        {/* page */}
+        <div className={cn('w-full overflow-auto', {"lg:w-[calc(100%-var(--left-side-bar-width))]": isLeftSidebarActive})}>
+          <div className="">{children}</div>
+        </div>
       </div>
     </div>
   );
 }
-
-
