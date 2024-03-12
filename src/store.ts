e5db7531b86b2c24af619350f3aafbb1d6d637ff
dc <00danzy@gmail.com>
2024-03-12 23:42:33 +0100

    feat: add rightDashboardSidebarStore

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,27 @@
 import { create } from 'zustand';
 
-
+// Main sidebar
 type leftSidebarStore = {
   isActive: boolean,
   isMobileActive: boolean,
   setIsActive: (isActive: boolean) => void
   setMobileIsActive: (isMobileActive: boolean) => void
 }
-
 export const useLeftSidebarStore = create<leftSidebarStore>((set) => ({
+  isActive: true,
+  isMobileActive: false,
+  setIsActive: (isActive: boolean) => set({isActive}),
+  setMobileIsActive: (isMobileActive: boolean) => set({isMobileActive}),
+}));
+
+// Dashboard right sidebar
+type rightDashboardSidebarStore = {
+  isActive: boolean,
+  isMobileActive: boolean,
+  setIsActive: (isActive: boolean) => void
+  setMobileIsActive: (isMobileActive: boolean) => void
+}
+export const useRightDashboardSidebarStore = create<rightDashboardSidebarStore>((set) => ({
   isActive: true,
   isMobileActive: false,
   setIsActive: (isActive: boolean) => set({isActive}),
