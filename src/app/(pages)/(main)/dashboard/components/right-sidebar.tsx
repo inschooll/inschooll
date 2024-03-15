a2710e1ad32bc1f807ceff0b1da98a28e0136752
dc <00danzy@gmail.com>
2024-03-15 22:48:11 +0100

    refactor: improve code structure

--- src/app/(pages)/(main)/dashboard/components/right-sidebar.tsx
+++ src/app/(pages)/(main)/dashboard/components/right-sidebar.tsx
@@ -3,18 +3,12 @@ import { PanelRight } from "lucide-react";
 import { useRightDashboardSidebarStore } from "~/store";
 
 export default function DashboardRightSidebar() {
-  const { isActive } = useRightDashboardSidebarStore();
-
   return (
-    <>
-      {isActive && (
-        <div className="h-screen w-[var(--right-dashboard-side-bar-width)] border-l border-cc-border">
-          <div className="flex h-[3.25rem] items-center justify-end pl-2 pr-4">
-            <RightDashboardSidebarToggleButton />
-          </div>
-        </div>
-      )}
-    </>
+    <div className="h-screen w-[var(--right-dashboard-side-bar-width)] border-l border-cc-border">
+      <div className="flex h-[3.25rem] items-center justify-end pl-2 pr-4">
+        <RightDashboardSidebarToggleButton />
+      </div>
+    </div>
   );
 }
 
