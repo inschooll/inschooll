2de73ca4c835195dffbcbf344b391704cd6573f4
dc <00danzy@gmail.com>
2024-03-16 14:59:55 +0100

    fix: dialog header to be more standard

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -44,12 +44,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-2.5 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <div className="hover:bg-secondary-hover rounded p-2">
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </div>
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
@@ -59,13 +53,20 @@ const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
-      className
-    )}
-    {...props}
-  />
+  <div className={cn("flex items-center justify-between px-5 border-b border-cc-border", className)}>
+    <div
+      className="flex flex-col space-y-1.5 text-center sm:text-left font-semibold"
+      {...props}
+      
+    />
+    <DialogPrimitive.Close className="rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+        <div className="app-hover rounded-md p-2">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </div>
+      </DialogPrimitive.Close>
+
+  </div>
 )
 DialogHeader.displayName = "DialogHeader"
 
