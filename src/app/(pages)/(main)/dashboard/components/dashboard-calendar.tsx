9b69c2c2e64fb1b44f55ea07ae62e37d8ed6fdd4
dc <00danzy@gmail.com>
2024-03-16 15:04:30 +0100

    styles: improve to fit well in the right sidebar

--- src/app/(pages)/(main)/dashboard/components/dashboard-calendar.tsx
+++ src/app/(pages)/(main)/dashboard/components/dashboard-calendar.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { useState } from "react";
 import { Calendar } from "~/components/ui/calendar";
+import { cn } from "~/lib/utils";
 
-export default function DashboardCalendar() {
+export default function DashboardCalendar({className} : {className: string}) {
     const [date, setDate] = useState<Date>();
   
     return (
-      <div className="mt-5">
+      <div className={cn("flex bg-amber-2000 justify-center", className)}>
         <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
           className=""
-          classNameCell="h-7 w-7"
+          classNameCell="h-[1.7rem] w-[2.25rem] "
         />
       </div>
     );
