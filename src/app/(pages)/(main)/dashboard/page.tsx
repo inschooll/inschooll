b8e74579b1d049321734b0ba40af05bc7b65db83
dc <00danzy@gmail.com>
2024-03-16 15:08:03 +0100

    refactor: improve code

--- src/app/(pages)/(main)/dashboard/page.tsx
+++ src/app/(pages)/(main)/dashboard/page.tsx
@@ -1,26 +1,21 @@
-import { T3 } from "~/components/texts/title";
-import { Button } from "~/components/ui/button";
-import icons from "~/lib/constants/icons";
 import DashboardNavbar from "./components/dashboard-navbar";
 
+import DashboardMainWrapper from "./components/dashboard-main-wrapper";
 import DashboardRightSidebar from "./components/right-sidebar";
 import Timeline from "./components/timeline";
 import "./page.css";
-import DashboardMainWrapper from "./components/dashboard-main-wrapper";
-import { currentDayName, currentMonthName, getDaySuffix } from "~/lib/utils";
 
 export default function Dashboard() {
+
   return (
     <div className="bg-green-5000">
-      {/* <main className="mainCvr"> */}
       <main className="">
         <div className="lg:flex">
           <DashboardMainWrapper>
             <DashboardNavbar />
-
             <Timeline />
           </DashboardMainWrapper>
-          {/* Right sidebar */}
+
           <DashboardRightSidebar />
         </div>
       </main>
