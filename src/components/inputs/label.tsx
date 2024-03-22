a639783ca2654d17b6cc53ecb20e5ecbc71d45a0
dc <00danzy@gmail.com>
2024-03-22 21:13:02 +0100

    fix: make isRequired prop optional ?

--- src/components/inputs/label.tsx
+++ src/components/inputs/label.tsx
@@ -1,4 +1,4 @@
-export default function Label({className, value, labelFor, isRequired} : {className?: string, value: string, labelFor?: string, isRequired: boolean}) {
+export default function Label({className, value, labelFor, isRequired} : {className?: string, value: string, labelFor?: string, isRequired?: boolean}) {
   return (
     <label htmlFor={labelFor} className={`text-sm font-medium text-cc-content/80 ${className}`}>
       {value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()}
