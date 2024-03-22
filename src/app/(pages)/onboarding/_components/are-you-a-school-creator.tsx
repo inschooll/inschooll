8b89d4f72a4c66cb5101d5638d142ad3db778c0a
dc <00danzy@gmail.com>
2024-03-22 21:16:48 +0100

    refactor: improve code

--- src/app/(pages)/onboarding/_components/are-you-a-school-creator.tsx
+++ src/app/(pages)/onboarding/_components/are-you-a-school-creator.tsx
@@ -5,6 +5,8 @@ import { ChooseContainer, OnboardingButton, OnboardingTitleAndDescription } from
 import SchoolBenefits from "./school-benefits";
 import { OnboardingContext } from "~/lib/context";
 
+
+const options = [<SchoolBenefits key={1} />, <>Hey</>];
 /**
  * This component asks users whether they want to create a school or not. And the next 
  * component displayed when the user clicks the continue button would depend on the 
@@ -12,9 +14,8 @@ import { OnboardingContext } from "~/lib/context";
  * @returns 
  */
 export default function AreYouASchoolCreator() {
-  const displayNextComponent = useContext(OnboardingContext);
-  const [selectedIndex, setSelectedIndex] = useState<number>();
-  const options = [<SchoolBenefits key={1} />, <>Hey</>];
+  const {displayNewComponent: displayNextComponent} = useContext(OnboardingContext);
+  const [selectedOptionsIndex, setSelectedOptionsIndex] = useState<number>();
 
   return (
     <>
@@ -27,15 +28,15 @@ export default function AreYouASchoolCreator() {
         <div className="rounded-lg pt-5 md:flex">
           <ChooseContainer
             className="rounded-t-sm sm:rounded-t-none md:rounded-l-lg"
-            isSelected={selectedIndex == 0}
-            onClick={setSelectedIndex.bind(null, 0)}
+            isSelected={selectedOptionsIndex == 0}
+            onClick={setSelectedOptionsIndex.bind(null, 0)}
           >
             <T2 className="text-cc-content/70">Yes</T2>
           </ChooseContainer>
           <ChooseContainer
             className="rounded-b-sm sm:rounded-b-none md:rounded-r-lg"
-            isSelected={selectedIndex == 1}
-            onClick={setSelectedIndex.bind(null, 1)}
+            isSelected={selectedOptionsIndex == 1}
+            onClick={setSelectedOptionsIndex.bind(null, 1)}
           >
             <T2 className="text-cc-content/70">No</T2>
           </ChooseContainer>
@@ -44,8 +45,8 @@ export default function AreYouASchoolCreator() {
 
       <OnboardingButton
         onClick={
-          selectedIndex != undefined
-            ? displayNextComponent.bind(null, options[selectedIndex])
+          selectedOptionsIndex != undefined
+            ? displayNextComponent?.bind(null, options[selectedOptionsIndex])
             : () => null
         }
       />
