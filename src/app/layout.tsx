2c9995cd65c8defd87ed602132fac3c34e86356a
dc <00danzy@gmail.com>
2024-03-22 21:19:10 +0100

    fix: integrate the use of getTheme and setTheme util functions 🕺

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import type { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import AppHotKeys from "~/components/app-hot-keys";
 import { TRPCReactProvider } from "~/trpc/react";
-import { getTheme } from "./actions";
 import { Toaster } from "~/components/ui/toaster";
+import { cookies } from "next/headers";
+import SetTheme from "~/components/set-theme";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,13 +25,14 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const theme = await getTheme()
+  const theme = cookies().get("theme")?.value;
 
   return (
     <html lang="en" data-theme={theme ?? "system"}>
       <body
         className={`h-full bg-cc-background font-sans text-cc-content ${inter.className} antialiased`}
       >
+        <SetTheme theme={theme} />
         <NextTopLoader showSpinner={false} color="#54a0ff" shadow={""} height={3}  />
         <AppHotKeys>
           <TRPCReactProvider>{children}</TRPCReactProvider>
@@ -40,4 +42,3 @@ export default async function RootLayout({
     </html>
   );
 }
-
