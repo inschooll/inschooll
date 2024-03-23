87354f47069ffca1378e74ed739c6de091dcdee3
dc <00danzy@gmail.com>
2024-03-23 06:21:19 +0100

    styles: add className prop

--- src/app/(pages)/onboarding/_components/onboarding-card.tsx
+++ src/app/(pages)/onboarding/_components/onboarding-card.tsx
@@ -1,6 +1,8 @@
-export default function OnboardingCard({ children }: { children: React.ReactNode }) {
+import { cn } from "~/lib/utils";
+
+export default function OnboardingCard({ className, children }: { className?: string, children: React.ReactNode }) {
   return (
-    <div className="app-box-shadow w-full divide-y divide-cc-border rounded-lg p-7 text-left">
+    <div className={cn("app-box-shadow w-full rounded-lg p-7 text-left md:w-[36rem] mx-auto", className)}>
       {children}
     </div>
   );
