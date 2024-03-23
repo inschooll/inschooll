c41afa97fff5b9bfe8de7c4a6ae1c59b47b79520
dc <00danzy@gmail.com>
2024-03-23 06:22:25 +0100

    refactor: improve code

--- src/app/(pages)/onboarding/_components/are-you-a-school-creator.tsx
+++ src/app/(pages)/onboarding/_components/are-you-a-school-creator.tsx
@@ -1,46 +1,48 @@
 "use client";
 import { useContext, useState } from "react";
 import { T2 } from "~/components/texts/title";
-import { ChooseContainer, OnboardingButton, OnboardingTitleAndDescription } from "../page";
+import {
+  ChooseContainer,
+  OnboardingButton,
+  OnboardingTitleAndDescription,
+} from "../page";
 import SchoolBenefits from "./school-benefits";
 import { OnboardingContext } from "~/lib/context";
 
-
 const options = [<SchoolBenefits key={1} />, <>Hey</>];
 /**
- * This component asks users whether they want to create a school or not. And the next 
- * component displayed when the user clicks the continue button would depend on the 
+ * This component asks users whether they want to create a school or not. And the next
+ * component displayed when the user clicks the continue button would depend on the
  * user selection
- * @returns 
+ * @returns
  */
 export default function AreYouASchoolCreator() {
-  const {displayNewComponent: displayNextComponent} = useContext(OnboardingContext);
+  const { displayNewComponent: displayNextComponent } =
+    useContext(OnboardingContext);
   const [selectedOptionsIndex, setSelectedOptionsIndex] = useState<number>();
 
   return (
     <>
-      <div className="space-y-8 text-center">
-        <OnboardingTitleAndDescription
-          title="Do you wish to create a School?"
-          description=""
-        />
+      <OnboardingTitleAndDescription
+        title="Do you wish to create a School?"
+        description=""
+      />
 
-        <div className="rounded-lg pt-5 md:flex">
-          <ChooseContainer
-            className="rounded-t-sm sm:rounded-t-none md:rounded-l-lg"
-            isSelected={selectedOptionsIndex == 0}
-            onClick={setSelectedOptionsIndex.bind(null, 0)}
-          >
-            <T2 className="text-cc-content/70">Yes</T2>
-          </ChooseContainer>
-          <ChooseContainer
-            className="rounded-b-sm sm:rounded-b-none md:rounded-r-lg"
-            isSelected={selectedOptionsIndex == 1}
-            onClick={setSelectedOptionsIndex.bind(null, 1)}
-          >
-            <T2 className="text-cc-content/70">No</T2>
-          </ChooseContainer>
-        </div>
+      <div className="rounded-lg pt-5 md:flex">
+        <ChooseContainer
+          className="rounded-t-sm sm:rounded-t-none md:rounded-l-lg"
+          isSelected={selectedOptionsIndex == 0}
+          onClick={setSelectedOptionsIndex.bind(null, 0)}
+        >
+          <T2 className="text-cc-content/70">Yes</T2>
+        </ChooseContainer>
+        <ChooseContainer
+          className="rounded-b-sm sm:rounded-b-none md:rounded-r-lg"
+          isSelected={selectedOptionsIndex == 1}
+          onClick={setSelectedOptionsIndex.bind(null, 1)}
+        >
+          <T2 className="text-cc-content/70">No</T2>
+        </ChooseContainer>
       </div>
 
       <OnboardingButton
