b8af2e14081753401fbc0449eaeb54ac688689c6
dc <00danzy@gmail.com>
2024-03-24 11:07:44 +0100

    styles: improve remove button width & height

--- src/components/buttons/composite-buttons.tsx
+++ src/components/buttons/composite-buttons.tsx
@@ -2,13 +2,15 @@ import { Plus } from "lucide-react";
 import { Button } from "../ui/button";
 import type { HTMLAttributes } from "react";
 import { CgRemove } from "react-icons/cg";
+import { cn } from "~/lib/utils";
 
 export const AddButton = ({
   title = "Add",
+  className,
   ...props
 }: HTMLAttributes<HTMLButtonElement>) => {
   return (
-    <Button variant={"secondary"} type="button" {...props}>
+    <Button variant={"secondary"} type="button" size={'sm'} className={cn("h-[2.375rem]", className)} {...props}>
       <div className="flex items-center space-x-1">
         <Plus size={18} />
         <span>{title}</span>
@@ -20,7 +22,7 @@ export const RemoveButton = ({
   ...props
 }: HTMLAttributes<HTMLButtonElement>) => {
   return (
-    <Button className="w-10 p-0" variant="secondary" type="button" {...props}>
+    <Button className="w-[2.375rem] p-0 h-[2.375rem]" variant="secondary" type="button" {...props}>
       <CgRemove className="scale-[1.2] text-cc-content/70 hover:text-cc-content/90" />
     </Button>
   );
