e48f00108d6b2b3d9263ef26df897d7cec528de2
dc <00danzy@gmail.com>
2024-03-25 10:03:02 +0100

    styles: use javascript to implement Title case

--- src/components/inputs/label.tsx
+++ src/components/inputs/label.tsx
@@ -2,8 +2,8 @@ import { cn } from "~/lib/utils";
 
 export default function Label({className, value, labelFor, isRequired} : {className?: string, value: string, labelFor?: string, isRequired?: boolean}) {
   return (
-    <label htmlFor={labelFor} className={cn("text-sm font-medium text-cc-content/80 capitalize", className)}>
-      {value}
+    <label htmlFor={labelFor} className={cn("text-sm font-medium text-cc-content/80", className)}>
+      {value[0]?.toLocaleUpperCase() + value.slice(1,).toLowerCase()}
       {isRequired && <span className="text-red-500"> *</span>}
     </label>
   );
